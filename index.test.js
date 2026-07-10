ed27e5bf8bd8ee4f8b74cb50f9ed699a2bc71408
krishan sain <krishan.sain4294@gmail.com>
2026-07-10 19:56:19 +0530

    break: missing secret

--- index.test.js
+++ index.test.js
@@ -1,4 +1,11 @@
-test('sanity check', () => {
-  expect(1 + 1).toBe(2);
+// test('sanity check', () => {
+//   expect(1 + 1).toBe(2);
   
+// });
+
+test('sanity check', () => {
+  if (!process.env.API_KEY) {
+    throw new Error('API_KEY is missing!');
+  }
+  expect(1 + 1).toBe(2);
 });
